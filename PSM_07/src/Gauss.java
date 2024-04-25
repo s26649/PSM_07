8ce2d089fc5c6047822d5809f0f5325c3bb8998b
Tokarska Oleksandra <oleksandra.tokarska@columbuspro.eu>
2024-04-25 10:55:42 +0200

    zapomnialam add..

--- PSM_07/src/Gauss.java
+++ PSM_07/src/Gauss.java
@@ -1,2 +1,45 @@
 public class Gauss {
+    private static final double EPSILON = 1e-10;
+
+    public static double[] solve(double[][] A, double[] B) {
+        int n = A.length;
+
+        for (int p = 0; p < n; p++) {
+            int max = p;
+            for (int i = p + 1; i < n; i++) {
+                if (Math.abs(A[i][p]) > Math.abs(A[max][p])) {
+                    max = i;
+                }
+            }
+            double[] temp = A[p];
+            A[p] = A[max];
+            A[max] = temp;
+            double t = B[p];
+            B[p] = B[max];
+            B[max] = t;
+
+            if (Math.abs(A[p][p]) <= EPSILON) {
+                throw new RuntimeException();
+            }
+
+            for (int i = p + 1; i < n; i++) {
+                double alpha = A[i][p] / A[p][p];
+                B[i] -= alpha * B[p];
+                for (int j = p; j < n; j++) {
+                    A[i][j] -= alpha * A[p][j];
+                }
+            }
+        }
+
+        double[] x = new double[n];
+        for (int i = n - 1; i >= 0; i--) {
+            double sum = 0.0;
+            for (int j = i + 1; j < n; j++) {
+                sum += A[i][j] * x[j];
+            }
+            x[i] = (B[i] - sum) / A[i][i];
+        }
+
+        return x;
+    }
 }
